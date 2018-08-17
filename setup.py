75eb5d757a348ed3b32db5c57757aa6a437104ba
jesjimher <jesjimenez@gmail.com>
2018-08-17 14:13:37 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jpegdupes",
-    version="2.0.10",
+    version="2.0.13",
     author="Jesús Jiménez",
     author_email="jesjimenez@gmail.com",
     description="Image duplicate finder that ignores JPEG metadata",
