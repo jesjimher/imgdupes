0178aee33ead05c1ae6d176df85a40c532c8ef92
jesjimher <jesjimenez@gmail.com>
2018-08-17 10:18:16 +0200

    More descriptors

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="imgdupes",
-    version="1.0",
+    version="2.0",
     author="Jesús Jiménez",
     author_email="jesjimenez@gmail.com",
-    description="Image duplicate finder that ignores metadata",
+    description="Image duplicate finder that ignores JPEG metadata",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords='image duplicate finder metadata jpeg',
     url="https://github.com/jesjimher/imgdupes",
     packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'texttable','jpegtran-cffi>0.5',
+        ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
